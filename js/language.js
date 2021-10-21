6a8c640db92e964d5f1676fba0f157dad3ac4546
Sebastian Ibagué Castro <sebastian.castro@binapps.co>
2021-10-21 11:21:28 -0500

    Se agrega el Proyecto

---
+++ js/language.js
@@ -0,0 +1,315 @@
+var dictionary = {
+    'Home':{
+        'en': 'Home',
+        'es': 'Inicio'
+    },
+    'About':{
+        'en': 'About',
+        'es': 'Acerca de'
+    },
+    'About us':{
+        'en': 'About us',
+        'es': 'Sobre nosotros'
+    },
+    'Candys':{
+        'en': 'Candys',
+        'es': 'Dulces'
+    },
+    'New':{
+        'en': 'New',
+        'es': 'Nuevo'
+    },
+    'News':{
+        'en': 'News',
+        'es': 'Noticias'
+    },
+    'Support':{
+        'en': 'Support',
+        'es': 'Soporte'
+    },
+    'Features':{
+        'en': 'Features',
+        'es': 'Características'
+    },
+    'Our Services':{
+        'en': 'Our Services',
+        'es': 'Nuestros Servicios'
+    },
+    'Pricing':{
+        'en': 'Pricing',
+        'es': 'Precios'
+    },
+    'Discounts':{
+        'en': 'Discounts',
+        'es': 'Descuentos'
+    },
+    'Shipping mode':{
+        'en': 'Shipping mode',
+        'es': 'Modo de envio'
+    },
+    'Our Company':{
+        'en': 'Our Company',
+        'es': 'Nuestra Compañía'
+    },
+    'Blog':{
+        'en': 'Blog',
+        'es': 'Blog'
+    },
+    'Our mission':{
+        'en': 'Our mission',
+        'es': 'Nuestra misión'
+    },
+    'Contact us':{
+        'en': 'Contact us',
+        'es': 'Contactanos'
+    },
+    'Subscribe':{
+        'en': 'Subscribe',
+        'es': 'Suscribirse'
+    },
+    'Our Newsletter':{
+        'en': 'Our Newsletter',
+        'es': 'Nuestro Boletín'
+    },
+    'New':{
+        'en': 'New',
+        'es': 'Nuevo'
+    },
+    'Accesory':{
+        'en': 'Accesory',
+        'es': 'Accesorio'
+    },
+    'Skull poetry':{
+        'en': 'Skull poetry',
+        'es': 'Poesía de calavera'
+    },
+    'Funko cat':{
+        'en': 'Funko cat',
+        'es': 'Funko de Gato'
+    },
+    'Butcher tree':{
+        'en': 'Butcher tree',
+        'es': 'Carnicero'
+    },
+    'Jason mask':{
+        'en': 'Jason mask',
+        'es': 'Máscara de Jason'
+    },
+    'Scarecrow':{
+        'en': 'Scarecrow',
+        'es': 'Espantapájaros'
+    },
+    'Treat':{
+        'en': 'Treat',
+        'es': 'Dulce'
+    },
+    'Candy apple':{
+        'en': 'Candy apple',
+        'es': 'Manzana acaramelada'
+    },
+    'Black cat':{
+        'en': 'Black cat',
+        'es': 'Gato negro'
+    },
+    'Pumpkin':{
+        'en': 'Pumpkin',
+        'es': 'Calabaza'
+    },
+    'Death':{
+        'en': 'Death',
+        'es': 'Muerte'
+    },
+    'Frankenstein':{
+        'en': 'Frankenstein',
+        'es': 'Frankenstein'
+    },
+    'Go to new':{
+        'en': 'Go to new',
+        'es': 'Ir a nuevo '
+    },
+    'New Arrivals':{
+        'en': 'New Arrivals',
+        'es': 'Los Recién Llegados'
+    },
+    'Candy':{
+        'en': 'Candy',
+        'es': 'Caramelo'
+    },
+    'Candies':{
+        'en': 'in Candies',
+        'es': 'en Caramelos'
+    },
+    'Monsters':{
+        'en': 'in Monsters',
+        'es': 'en Monstruos'
+    },
+    'Accesories':{
+        'en': 'in Accesories',
+        'es': 'en Accesorios'
+    },
+    '50% Discount':{
+        'en': '50% Discount',
+        'es': '50% de descuento'
+    },
+    'On New Products':{
+        'en': 'On New Products',
+        'es': 'En productos nuevos'
+    },
+    'Trick or Treat':{
+        'en': 'Trick or Treat',
+        'es': 'Dulce o Truco'
+    },
+    'TRICK OR':{
+        'en': 'TRICK OR',
+        'es': 'DULCE O'
+    },
+    'TREAT!!':{
+        'en': 'TREAT!!',
+        'es': 'TRUCO!!'
+    },
+    'Owl':{
+        'en': 'Owl',
+        'es': 'Búho'
+    },
+    'know more':{
+        'en': 'Know more',
+        'es': 'Saber más'
+    },
+    'Category':{
+        'en': 'Category',
+        'es': 'Categoría'
+    },
+    'Favorite Scare':{
+        'en': 'Favorite Scare',
+        'es': 'Susto Favorito'
+    },
+    'Ghosts':{
+        'en': 'Ghosts',
+        'es': 'Fantasma'
+    },
+    'Pumpkins':{
+        'en': 'Pumpkins',
+        'es': 'Calabazas'
+    },
+    'Witch Hat':{
+        'en': 'Witch Hat',
+        'es': 'Sombrero de Bruja'
+    },
+    'Book Now':{
+        'en': 'Book Now',
+        'es': 'Reservar Ahora'
+    },
+    'Terrifying Frankenstein':{
+        'en': 'Terrifying Frankenstein',
+        'es': 'Terrible Frankenstein'
+    },
+    'Scary Monster':{
+        'en': 'Scary Monster',
+        'es': 'Monstruo Aterrador'
+    },
+    'Green Cat':{
+        'en': 'Green Cat',
+        'es': 'Gato Verde'
+    },
+    'Scary Animal':{
+        'en': 'Scary Animal',
+        'es': 'Animal Aterrador'
+    },
+    'Voodoo Doll':{
+        'en': 'Voodoo Doll',
+        'es': 'Muñeco de Vudú'
+    },
+    'Possessed Doll':{
+        'en': 'Possessed Doll',
+        'es': 'Muñeco Poseído'
+    },
+    'REAPPEAR IN':{
+        'en': 'REAPPEAR IN',
+        'es': 'REAPARECE'
+    },
+    'FRONT OF':{
+        'en': 'FRONT OF',
+        'es': 'FRENTE A'
+    },
+    'YOU!!':{
+        'en': 'YOU!!',
+        'es': 'TI!!'
+    },
+    'BRING BACK':{
+        'en': 'BRING BACK',
+        'es': 'TRAE MI'
+    },
+    'MY CARAMEL':{
+        'en': 'MY CARAMEL',
+        'es': 'HILO DE'
+    },
+    'YARN!!':{
+        'en': 'YARN!!',
+        'es': 'CARAMELO!!'
+    },
+    'All rigths reserved.':{
+        'en': 'All rigths reserved.',
+        'es': 'Todos los derechos reservados.'
+    },
+    'Choose the ghost, the scariest there are.':{
+        'en': 'Choose the ghost, the scariest there are.',
+        'es': 'Elige el fantasma, el más aterrador que hay.'
+    },
+    'You look at the scariest pumpkins there is.':{
+        'en': 'You look at the scariest pumpkins there is.',
+        'es': 'Miras las calabazas más aterradoras que hay.'
+    },
+    'Pick the most stylish witch hats our there.':{
+        'en': 'Pick the most stylish witch hats our there.',
+        'es': 'Elige los sombreros de bruja más elegantes que hay.'
+    },
+    'Promotion new products and sales. Directly to your inbox.':{
+        'en': 'Promotion new products and sales. Directly to your inbox.',
+        'es': 'Promoción de nuevos productos y ventas. Directamente a su bandeja de entrada.'
+    },
+    'Enjoy the scariest night of your life.':{
+        'en': 'Enjoy the scariest night of your life.',
+        'es': 'Disfruta la noche más aterradora de tu vida.'
+    },
+    'Night of all the saints, or all the dead, is celebrated on October 31 and it is a very fun international celebration, this celebration comes from ancient origins and is already celebrated by everyone.':{
+        'en': 'Night of all the saints, or all the dead, is celebrated on October 31 and it is a very fun international celebration, this celebration comes from ancient origins and is already celebrated by everyone.',
+        'es': 'La noche de todos los santos, o de todos los muertos, se celebra el 31 de octubre y es una celebración internacional muy divertida, esta celebración viene de orígenes ancestrales y ya es celebrada por todos.'
+    },
+    'It is a being created from different parts of corpses, which is given life by Victor Frankenstein (its creator) during an experiment.':{
+        'en': 'It is a being created from different parts of corpses, which is given life by Victor Frankenstein (its creator) during an experiment.',
+        'es': 'Es un ser creado a partir de diferentes partes de cadáveres, al que Victor Frankenstein (su creador) le da vida durante un experimento.'
+    },
+    'Their characteristics and capabilities, in many cases extraordinary, have caused problems for those who, seeing them as dark entities.':{
+        'en': 'Their characteristics and capabilities, in many cases extraordinary, have caused problems for those who, seeing them as dark entities.',
+        'es': 'Sus características y capacidades, en muchos casos extraordinarias, han causado problemas a quienes las ven como entidades oscuras.'
+    },
+    'Voodoo is a religion native to West Africa, where it is still practiced today by members of the Ewé, Kabye, Mina and Fon ethnic groups from Togo and Benin.':{
+        'en': 'Voodoo is a religion native to West Africa, where it is still practiced today by members of the Ewé, Kabye, Mina and Fon ethnic groups from Togo and Benin.',
+        'es': 'El vudú es una religión originaria de África occidental, donde todavía lo practican miembros de las etnias Ewé, Kabye, Mina y Fon de Togo y Benin.'
+    }
+    
+};
+
+var langs = ['en', 'es'];
+var current_lang_index = 0;
+var current_lang = langs[current_lang_index];
+var varTranslator = true;
+
+window.change_lang = function() {
+    current_lang_index = ++current_lang_index % 2;
+    current_lang = langs[current_lang_index];
+    translate();
+}
+
+function translate() {
+    var newsletterInput = document.getElementById('newsletter__input');
+    varTranslator?newsletterInput.placeholder = "Enter your email":newsletterInput.placeholder = "Introduce tu correo electrónico";
+    varTranslator=!varTranslator;
+
+    $("[data-translate]").each(function(){
+        var key = $(this).data('translate');
+        $(this).html(dictionary[key][current_lang] || "N/A");
+    });
+}
+
+translate();
